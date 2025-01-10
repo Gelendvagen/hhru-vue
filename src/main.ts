e4349306ca143d7c9cafda9f33b410d00e1ff108
Angelina_Prokina <angelinaprokina@yandex.ru>
2025-01-10 22:46:13 +0300

    add page list

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,17 @@ import 'primeflex/primeflex.css'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
 import Toast from 'primevue/toast'
-import ToastService from 'primevue/toastservice'
 import ProgressSpinner from 'primevue/ProgressSpinner'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import ConfirmDialog from 'primevue/confirmdialog'
+import InlineMessage from 'primevue/inlinemessage'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import ConfirmationService from 'primevue/confirmationservice'
 
 import App from './App.vue'
 import router from './router'
@@ -19,6 +24,7 @@ import { Menubar } from 'primevue'
 const app = createApp(App)
 
 app.use(ToastService)
+app.use(ConfirmationService)
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue)
@@ -27,5 +33,9 @@ app.component('app-button', Button)
 app.component('app-input-text', InputText)
 app.component('app-toast', Toast)
 app.component('app-progress', ProgressSpinner)
+app.component('app-datatable', DataTable)
+app.component('app-column', Column)
+app.component('app-dialog', ConfirmDialog)
+app.component('app-message', InlineMessage)
 
 app.mount('#app')
