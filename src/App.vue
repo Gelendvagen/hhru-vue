c00f1277190709ae35529ee3694d3eaf81a1cf52
Angelina_Prokina <angelinaprokina@yandex.ru>
2025-02-02 23:23:48 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -27,3 +27,11 @@
         <div class="content"><RouterView /></div>
     </div>
 </template>
+
+<style scoped>
+    .container {
+        max-width: 1280px;
+        margin: auto;
+        padding: 20px;
+    }
+</style>
